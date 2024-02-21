39ff1f8bb1b96b7b5c5ade09efb685fbc13f8e87
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-21 16:32:50 -0500

    firewall & instance

--- variables.tf
+++ variables.tf
@@ -1,34 +1,63 @@
-variable "project_id" {
-  description = "GCP project ID"
-}
-
-variable "region" {
-  description = "GCP region"
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
 }
 
 variable "credentials_file" {
-  description = "Path to GCP service account credentials JSON file"
+  description = "Path to the Google Cloud credentials file"
+}
+
+variable "project_id" {
+  description = "Google Cloud Project ID"
+}
+
+variable "region" {
+  description = "Google Cloud region"
 }
 
 variable "vpc_name" {
   description = "Name of the VPC"
-  default     = "my-vpc"
+}
+
+variable "routing_mode" {
+  description = "Routing mode for the VPC"
+  default     = "REGIONAL"
 }
 
 variable "webapp_subnet_cidr" {
-  description = "CIDR range for the webapp subnet"
-  default     = "100.1.1.0/24"
+  description = "CIDR range for the webapp subnetwork"
 }
 
 variable "db_subnet_cidr" {
-  description = "CIDR range for the db subnet"
-  default     = "100.1.2.0/24"
+  description = "CIDR range for the db subnetwork"
 }
-
 
 variable "default_route_dest_range" {
-  description = "Destination IP range for the default route"
-  default     = "0.0.0.0/0"
+  description = "Destination range for the default route"
 }
 
+variable "machine_type" {
+  description = "Machine type for the instance"
+  default     = "e2-medium"
+}
 
+variable "zone" {
+  description = "Zone for the instance"
+  default     = "us-east1-b"
+}
+
+variable "image" {
+  description = "Image for the instance"
+  default     = "projects/network-cloud-002277864/global/images/web-app-20240221182725"
+}
+
+variable "size" {
+  description = "Size for the instance's boot disk"
+  default     = "20"
+}
+
+variable "type" {
+  description = "Type for the instance's boot disk"
+  default     = "pd-balanced"
+}
