9bc65614559c36c414bc72ea8e6c1a875f42eab1
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-21 16:32:50 -0500

    firewall & instance

--- main.tf
+++ main.tf
@@ -5,9 +5,10 @@ provider "google" {
 }
 
 resource "google_compute_network" "my_vpc" {
-  name                    = var.vpc_name
+  name                    = "${var.environment}-${var.vpc_name}"
   auto_create_subnetworks = false
-  routing_mode            = "
+  routing_mode            = var.routing_mode
+  delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
@@ -23,10 +24,57 @@ resource "google_compute_subnetwork" "db" {
 }
 
 resource "google_compute_route" "default_route" {
-  name                  = "default-route"
+  name                  = "${var.environment}-default-route"
   network               = google_compute_network.my_vpc.name
   dest_range            = var.default_route_dest_range
-  next_hop_gateway      = google_compute_network.my_vpc.gateway_ipv4
+  next_hop_gateway      = "default-internet-gateway"
 }
 
+resource "google_compute_firewall" "allow_internet" {
+  name    = "${var.environment}-allow-internet"
+  network = google_compute_network.my_vpc.name
 
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]  
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-internet"]
+}
+
+resource "google_compute_firewall" "deny_ssh" {
+  name    = "${var.environment}-deny-ssh"
+  network = google_compute_network.my_vpc.name
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"] 
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "myinstance" {
+  name = "${var.environment}-webapp-${formatdate("YYYYMMDDHHmmss", timestamp())}"
+
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.webapp.name
+    access_config {
+      network_tier = "PREMIUM"
+    }
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.size
+      type  = var.type
+    }
+  }
+}
