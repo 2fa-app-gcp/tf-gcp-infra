1620a493d6c4a801011155f3f1ac1e998df6d56b
shobaraajeswar <shoban@Shobaraajeswars-MacBook-Air.local>
2024-02-28 12:36:18 -0500

    initial tf

--- main.tf
+++ main.tf
@@ -42,23 +42,45 @@ resource "google_compute_firewall" "allow_internet" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_firewall" "deny_ssh" {
-  name    = "${var.environment}-deny-ssh"
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "${var.environment}-allow-ssh"
   network = google_compute_network.my_vpc.name
-  priority = 1000
-  deny {
+  priority = 801
+  allow {
     protocol = "tcp"
+    ports    = ["22"]  
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
 
+# resource "google_compute_firewall" "deny_ssh" {
+#   name    = "${var.environment}-deny-ssh"
+#   network = google_compute_network.my_vpc.name
+#   priority = 1000
+#   deny {
+#     protocol = "tcp"
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+# }
+
 resource "google_compute_instance" "myinstance" {
   name = "${var.environment}-webapp-${formatdate("YYYYMMDDHHmmss", timestamp())}"
-
   machine_type = var.machine_type
   zone         = var.zone
+  depends_on = [ google_sql_user.users ]
 
+   metadata_startup_script = <<-SCRIPT
+    #!/bin/bash
+    # Download secrets file and place it in /opt/webapp/secrets
+    mkdir -p /opt/webapp/secrets
+    
+    rm -f /opt/webapp/secrets/secrets.env && echo "HOST=${google_sql_database_instance.main.private_ip_address}" > /opt/webapp/secrets/secrets.env
+    echo "USERNAME=${google_sql_user.users.name}" >> /opt/webapp/secrets/secrets.env
+    echo "PASSWORD=${google_sql_user.users.password}" >> /opt/webapp/secrets/secrets.env
+    echo "DATABASE=${google_sql_database.main.name}">> /opt/webapp/secrets/secrets.env
+  SCRIPT
   network_interface {
     subnetwork = google_compute_subnetwork.webapp.name
     access_config {
@@ -74,3 +96,57 @@ resource "google_compute_instance" "myinstance" {
     }
   }
 }
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  project       = var.project_id
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.my_vpc.name
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.my_vpc.name
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_sql_database_instance" "main" {
+  name             = "main-instance-${random_id.db_name_suffix.hex}"
+  database_version = "MYSQL_8_0"
+  depends_on = [ google_service_networking_connection.private_vpc_connection ]
+  region     = var.region
+  deletion_protection= false
+  
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = false
+      private_network =  google_compute_network.my_vpc.self_link
+      enable_private_path_for_google_cloud_services = true
+    }
+  }
+}
+
+resource "google_sql_database" "main" {
+  name       = "webapp"
+  instance   = google_sql_database_instance.main.name
+  project    = google_sql_database_instance.main.project
+}
+resource "random_password" "pwd" {
+  length  = 16
+  special = false
+}
+
+resource "google_sql_user" "users" {
+  name     = "user"
+  instance = google_sql_database_instance.main.name
+  password = random_password.pwd.result
+  depends_on = [google_sql_database.main] 
+}
+
